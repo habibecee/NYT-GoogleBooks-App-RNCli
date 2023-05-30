600a46d0f0ba36d59d166266330c22826572bcec
habibecee <habibe.ce1996@gmail.com>
2023-05-30 19:28:34 +0300

    account page edited

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -2,18 +2,14 @@ import React from 'react';
 import AnimatedLottieView from 'lottie-react-native';
 import {TouchableOpacity} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
-import {GeneralStyles} from '../Utils/GeneralStyles';
 
-export default function Avatar({style}) {
+export default function Avatar({style, source}) {
   const Navigation = useNavigation();
   return (
     <TouchableOpacity
       style={style}
       onPress={() => Navigation.navigate('Account')}>
-      <AnimatedLottieView
-        source={require('../../assets/animations/simpleUserIcon.json')}
-        autoPlay
-      />
+      <AnimatedLottieView source={source} autoPlay />
     </TouchableOpacity>
   );
 }
--- src/pages/Account/index.js
+++ src/pages/Account/index.js
@@ -1,17 +1,74 @@
-import {View, Text, StyleSheet, Button} from 'react-native';
+import {View, Text, StyleSheet, Button, TouchableOpacity} from 'react-native';
 import React from 'react';
 import {useNavigation} from '@react-navigation/native';
-import {GeneralStyles} from '../../Utils/GeneralStyles';
+import {GeneralStyles, colors, fonts} from '../../Utils/GeneralStyles';
+import Avatar from '../../components/Avatar';
 
 export default function Account() {
   const {navigate} = useNavigation();
   return (
-    <View style={GeneralStyles.container}>
-      <Text>Account</Text>
-      <Button title="Log In" onPress={() => navigate('LogIn')} />
-      <Button title="Sign In" onPress={() => navigate('SignIn')} />
+    <View style={[GeneralStyles.container, styles.container]}>
+      <Avatar
+        style={styles.Avatar}
+        source={require('../../../assets/animations/searchUsers.json')}
+      />
+      <View style={styles.Button}>
+        <Text style={styles.SubText}>Do you have an account? </Text>
+
+        <TouchableOpacity
+          style={[styles.ButtonContainer, {backgroundColor: colors.dark}]}
+          onPress={() => navigate('LogIn')}>
+          <Text style={styles.ButtonText}>Log In</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.Button}>
+        <Text style={styles.SubText}> Or? </Text>
+
+        <TouchableOpacity
+          style={[styles.ButtonContainer, {backgroundColor: colors.primary}]}
+          onPress={() => navigate('SignIn')}>
+          <Text style={styles.ButtonText}>Sign In</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+    justifyContent: 'center',
+  },
+
+  Avatar: {
+    backgroundColor: colors.secondary,
+    alignSelf: 'center',
+    marginBottom: 20,
+    width: 150,
+    height: 150,
+    borderRadius: 100,
+  },
+
+  SubText: {
+    fontFamily: fonts.bold,
+    fontSize: 18,
+    color: colors.dark,
+    textAlign: 'center',
+    marginBottom: 20,
+  },
+
+  Button: {
+    marginBottom: 15,
+  },
+
+  ButtonContainer: {
+    padding: 10,
+    borderRadius: 10,
+  },
+  ButtonText: {
+    fontFamily: fonts.bold,
+    fontSize: 20,
+    color: 'white',
+    textAlign: 'center',
+  },
+});
